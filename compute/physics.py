3f7bf726b576544fe587d91a8cb77dd4053ecccb
Vihaan Mehta <133607593+SoftwareDeveloperVM@users.noreply.github.com>
2026-01-10 20:24:25 -0800

    Add files via upload

---
+++ compute/physics.py
@@ -0,0 +1,45 @@
+gravity_constant = 6.674 * 10 ** -11
+
+
+# kinematics equations
+def final_velocity(initial_velocity, acceleration, time):
+    return initial_velocity + (acceleration * time)
+
+
+def displacement(initial_velocity, acceleration, time):
+    return initial_velocity * time + 0.5 * acceleration * time ** 2
+
+
+def velocity_squared(initial_velocity, acceleration, displacement):
+    return 2 * acceleration * displacement + initial_velocity ** 2
+
+
+def average_velocity(initial_velocity, final_velocity):
+    return (initial_velocity + final_velocity) / 2
+
+
+def thrust(mass_flow_rate, exhaust_velocity, exhaust_pressure, ambient_pressure, nozzle_exit_area):
+    return mass_flow_rate * exhaust_velocity + nozzle_exit_area * (exhaust_pressure - ambient_pressure)
+
+
+def force(mass, acceleration):
+    return mass * acceleration
+
+
+def universal_graviation(mass1, mass2, distance):
+    return gravity_constant * ((mass1 * mass2) / distance ** 2)
+
+
+def vector(direction, magnitude):
+    return [direction, magnitude]
+
+
+# linear algebra
+def matrix(rows, columns, characters=0):
+    matrix = []
+    for element in range(rows):
+        row = []
+        for element in range(columns):
+            row.append(characters)
+        matrix.append(row)
+    print(matrix)
